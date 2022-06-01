59366888a752493ae4fa07a425932ee3d0c56364
lucasellery <olucasellery@gmail.com>
2022-06-01 00:03:09 -0300

    responsive

--- src/components/ProfileBoxes/Technologies.tsx
+++ src/components/ProfileBoxes/Technologies.tsx
@@ -12,7 +12,7 @@ const Technologies: React.FC = () => {
     <div className="bg-brand-500 w-full h-auto rounded-2xl p-8 flex flex-col drop-shadow-lg">
       <Title title="Tecnologias" />
 
-      <div className="grid grid-cols-3 gap-3 mt-5">
+      <div className="grid gap-3 mt-5 sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-3">
         {technologiesList?.map((technology: ITechs) => (
           <span
             className="flex items-center justify-center p-2 rounded-3xl font-bold text-xs uppercase text-center"
--- src/components/ProfileColumn.tsx
+++ src/components/ProfileColumn.tsx
@@ -6,7 +6,7 @@ import WorkInfo from "./ProfileBoxes/WorkInfo";
 
 export function ProfileColumn() {
   return (
-    <div className="row-span-2 md:w-3/12">
+    <div className="row-span-2 lg:w-3/12 md:w-full">
       <PersonalInfo />
       <WorkInfo />
       <Technologies />
